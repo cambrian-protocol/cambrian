7e2b0a56763b70ea7ba36b1f7e9a0601f2efcca7
Nic Wickman <nicwickman@gmail.com>
2022-02-01 05:55:14 -0800

    cleanup

--- packages/app/pages/solvers/solver.tsx
+++ packages/app/pages/solvers/solver.tsx
@@ -1,20 +1,13 @@
 import Solver from '@cambrian/app/components/solver/Solver'
-import { SolidityDataTypes } from '@cambrian/app/models/SolidityDataTypes'
-import { getBytes32FromMultihash } from '@cambrian/app/utils/helpers/multihash'
+
 import React from 'react'
 import { UserContext } from '@cambrian/app/store/UserContext'
 
 const SOLVER_ABI = require('@artifacts/contracts/Solver.sol/Solver.json').abi
-const SOLVER_FACTORY_ABI =
-    require('@artifacts/contracts/SolverFactory.sol/SolverFactory.json').abi
 
 export default function SolverPage() {
     const user = React.useContext(UserContext)
     const address = '0xTODO'
-
-    // new ethers.providers.JsonRpcProvider(
-    //     'http://127.0.0.1:8545/' // local hardhat
-    // )
 
     if (user.currentSigner) {
         return (
@@ -25,6 +18,7 @@ export default function SolverPage() {
             />
         )
     } else {
-        return <>NOPE</>
+        // Metamask prompt?
+        return null
     }
 }
