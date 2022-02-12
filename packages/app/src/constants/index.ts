c73feef2da211b7c2d2fcc87a1cb2ebf63bcce64
Nic Wickman <nicwickman@gmail.com>
2022-02-11 22:55:28 -0800

    Add asking price + token suggestion and some input validation to templates/create page

--- packages/app/src/constants/index.ts
+++ packages/app/src/constants/index.ts
@@ -5,3 +5,5 @@ export const MAX_POINTS = 10000
 export const DEFAULT_ABI = BasicSolverArtifact.abi
 export const DEFAULT_IFACE = new utils.Interface(BasicSolverArtifact.abi)
 export const CERAMIC_NETWORK = 'https://127.0.0.1:7001'
+export const PIN_ENDPOINT =
+    'https://kdjzxk3x7a.execute-api.us-east-1.amazonaws.com/pinPinata'
