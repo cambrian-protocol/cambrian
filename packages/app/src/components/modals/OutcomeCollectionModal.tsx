5cd11cc26aebc51c6d6504aa85f5b48fa013954e
Hensohimself <oliver.hens@gmail.com>
2022-03-21 08:05:01 -0500

    Added missing Token drill

--- packages/app/src/components/modals/OutcomeCollectionModal.tsx
+++ packages/app/src/components/modals/OutcomeCollectionModal.tsx
@@ -4,15 +4,18 @@ import { Box } from 'grommet'
 import HeaderTextSection from '../sections/HeaderTextSection'
 import OutcomeCollectionCard from '../cards/OutcomeCollectionCard'
 import { OutcomeCollectionModel } from '@cambrian/app/models/OutcomeCollectionModel'
+import { TokenModel } from '@cambrian/app/models/TokenModel'
 
 type OutcomeCollectionModalProps = BaseLayerModalProps & {
     outcomeCollections: OutcomeCollectionModel[]
     proposeMethod?: (indexSet: number) => void
+    token: TokenModel
 }
 
 const OutcomeCollectionModal = ({
     proposeMethod,
     outcomeCollections,
+    token,
     ...rest
 }: OutcomeCollectionModalProps) => {
     return (
@@ -25,6 +28,7 @@ const OutcomeCollectionModal = ({
                 {outcomeCollections.map((outcomeCollection) => {
                     return (
                         <OutcomeCollectionCard
+                            token={token}
                             key={outcomeCollection.indexSet}
                             outcomeCollection={outcomeCollection}
                             proposeMethod={proposeMethod}
