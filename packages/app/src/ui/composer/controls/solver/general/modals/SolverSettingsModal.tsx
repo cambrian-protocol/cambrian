e79b955bb79929e883b00950d53978a8d09f03dc
Hensohimself <oliver.hens@gmail.com>
2022-03-24 14:35:36 -0500

    Added missing SolverTagModal integration

--- packages/app/src/ui/composer/controls/solver/general/modals/SolverSettingsModal.tsx
+++ packages/app/src/ui/composer/controls/solver/general/modals/SolverSettingsModal.tsx
@@ -1,25 +1,48 @@
 import BaseLayerModal from '@cambrian/app/components/modals/BaseLayerModal'
+import BaseMenuListItem from '@cambrian/app/components/buttons/BaseMenuListItem'
 import { Box } from 'grommet'
 import ComposerSolver from '@cambrian/app/classes/ComposerSolver'
 import HeaderTextSection from '@cambrian/app/components/sections/HeaderTextSection'
 import SolverSettingsForm from '../forms/SolverSettingsForm'
+import SolverTagModal from './SolverTagModal'
+import { Tag } from 'phosphor-react'
+import { useState } from 'react'
 
 type UpdateSlotFormModalProps = {
     onClose: () => void
     solver: ComposerSolver
 }
 
-const SolverSettingsModal = ({ onClose, solver }: UpdateSlotFormModalProps) => (
-    <BaseLayerModal onClose={onClose}>
-        <HeaderTextSection
-            title="Solver Settings"
-            subTitle={`Solver ID: ${solver.id}`}
-            paragraph="Define the Solver implementation, Keeper, Arbitrator, Timelock, and metadata."
-        />
-        <Box fill>
-            <SolverSettingsForm solver={solver} onClose={onClose} />
-        </Box>
-    </BaseLayerModal>
-)
+const SolverSettingsModal = ({ onClose, solver }: UpdateSlotFormModalProps) => {
+    const [showSolverTagModal, setShowSolverTagModal] = useState(false)
+
+    const toggleShowSolverTagModal = () =>
+        setShowSolverTagModal(!showSolverTagModal)
+    return (
+        <>
+            <BaseLayerModal onClose={onClose}>
+                <HeaderTextSection
+                    title="Solver Settings"
+                    subTitle={`Solver ID: ${solver.id}`}
+                    paragraph="Define the Solver implementation, Keeper, Arbitrator, Timelock, and metadata."
+                />
+                <Box fill gap="medium">
+                    <BaseMenuListItem
+                        title="Solver Tag"
+                        icon={<Tag />}
+                        onClick={toggleShowSolverTagModal}
+                    />
+                    <SolverSettingsForm solver={solver} onClose={onClose} />
+                </Box>
+            </BaseLayerModal>
+            {showSolverTagModal && (
+                <SolverTagModal
+                    currentSolverTag={solver.solverTag}
+                    onBack={toggleShowSolverTagModal}
+                />
+            )}
+        </>
+    )
+}
 
 export default SolverSettingsModal
