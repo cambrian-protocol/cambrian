52ed7e611a403cd35893b857825b158b025bdb42
Hensohimself <oliver.hens@gmail.com>
2022-04-09 07:49:48 -0500

    FlexInput styling updated

--- packages/app/src/components/inputs/FlexInput.tsx
+++ packages/app/src/components/inputs/FlexInput.tsx
@@ -1,8 +1,8 @@
 import { Box } from 'grommet'
 import { CheckBox } from 'grommet'
 import { FormField } from 'grommet'
-import InfoDropButton from '../buttons/InfoDropButton'
 import { TaggedInput } from '@cambrian/app/models/SlotTagModel'
+import { Text } from 'grommet'
 
 interface FlexInputProps {
     required?: boolean
@@ -21,36 +21,42 @@ const FlexInput = ({
     setFlexInputValue,
 }: FlexInputProps) => {
     return (
-        <Box direction="row" justify="between" gap="small">
-            <Box flex>
-                <FormField
-                    name={input.id}
-                    label={input.label}
-                    type={inputType}
-                    onChange={(event) =>
-                        setFlexInputValue(
-                            solverId,
-                            input.id,
-                            event.target.value
-                        )
-                    }
-                />
-            </Box>
-            {setIsFlex && (
-                <Box basis="1/4" justify="center" align="center">
-                    <CheckBox
-                        checked={input.isFlex}
-                        label="Leave Unfilled"
+        <Box pad={{ vertical: 'small' }}>
+            <Box direction="row" justify="between" gap="small">
+                <Box flex>
+                    <FormField
+                        name={input.id}
+                        label={input.label}
+                        type={inputType}
                         onChange={(event) =>
-                            setIsFlex(solverId, input.id, event.target.checked)
+                            setFlexInputValue(
+                                solverId,
+                                input.id,
+                                event.target.value
+                            )
                         }
                     />
                 </Box>
-            )}
+                {setIsFlex && (
+                    <Box basis="1/4" justify="center" align="center">
+                        <CheckBox
+                            checked={input.isFlex}
+                            label="Leave Unfilled"
+                            onChange={(event) =>
+                                setIsFlex(
+                                    solverId,
+                                    input.id,
+                                    event.target.checked
+                                )
+                            }
+                        />
+                    </Box>
+                )}
+            </Box>
             {input.description !== '' && (
-                <Box align="center" justify="center">
-                    <InfoDropButton info={input.description} />
-                </Box>
+                <Text size="xsmall" color="dark-4">
+                    {input.description}
+                </Text>
             )}
         </Box>
     )
