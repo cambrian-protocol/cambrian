f66b7b217645784177bfe73738de488b65321098
Hensohimself <oliver.hens@gmail.com>
2022-04-26 07:35:18 -0500

    Added Beta Warning Banner

--- packages/app/src/components/containers/ChainWarningContainer.tsx
+++ packages/app/src/components/containers/ChainWarningContainer.tsx
@@ -1,31 +1,32 @@
 import { Box, Text } from 'grommet'
+import { IconContext, Warning } from 'phosphor-react'
 
-import { Warning } from 'phosphor-react'
-
-// TODO Change on Prod / Link to list of supported chains
-const ChainWarningContainer = () => {
-    return (
-        <Box pad="small">
-            <Box
-                pad="medium"
-                background="status-warning"
-                round="small"
-                align="center"
-                justify="center"
-                direction="row"
-                gap="small"
-                elevation="small"
-            >
-                <Box width={{ min: 'auto' }}>
-                    <Warning size="24" />
-                </Box>
-                <Text>
-                    Warning, you are connected to an unsupported Chain. Please
-                    connect to the Ropsten Test Network.
-                </Text>
-            </Box>
-        </Box>
-    )
+interface WarningBannerProps {
+    message: string
+    icon?: JSX.Element
 }
 
-export default ChainWarningContainer
+// TODO Change on Prod / Link to list of supported chains
+const WarningBanner = ({ message, icon }: WarningBannerProps) => (
+    <Box pad="small">
+        <Box
+            pad="medium"
+            background="status-warning"
+            round="small"
+            align="center"
+            justify="center"
+            direction="row"
+            gap="small"
+            elevation="small"
+        >
+            <Box width={{ min: 'auto' }}>
+                <IconContext.Provider value={{ size: '24' }}>
+                    {icon ? icon : <Warning />}
+                </IconContext.Provider>
+            </Box>
+            <Text>{message}</Text>
+        </Box>
+    </Box>
+)
+
+export default WarningBanner
