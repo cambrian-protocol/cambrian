dc2d74b6af8e8567ead804c2be44ed6e657a97b9
Nic Wickman <nicwickman@gmail.com>
2022-05-03 19:27:52 -0700

    disable warning banner for now, minor text updates to CreateProposalUI

--- packages/app/config/index.ts
+++ packages/app/config/index.ts
@@ -1,10 +1,12 @@
-export const SHOW_BETA_WARNING = true
+export const SHOW_BETA_WARNING = false
 export const INFURA_ID = '811d34e940cf46b18bb8788535af23a2'
 export const FALLBACK_RPC_URL =
     'https://ropsten.infura.io/v3/5e5e73b367364266b008aed15e5b3189' // TODO Change to main, once we're on main
 
 export const IPFS_PIN_ENDPOINT =
-    'https://app.cambrianprotocol.com/api/pinPinata'
+    process.env.NODE_ENV == 'development'
+        ? 'https://kdjzxk3x7a.execute-api.us-east-1.amazonaws.com/pinPinata'
+        : 'https://app.cambrianprotocol.com/api/pinPinata'
 export const IPFS_GATEWAYS = [
     'cambrianprotocol.mypinata.cloud',
     'ipfs.dweb.link',
