7c7e3a51aa4e483eeeb68840df488de7d9a2e5d9
Hensohimself <oliver.hens@gmail.com>
2022-05-17 12:15:38 -0500

    Added USPListSection

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Box } from 'grommet'
+import Glow from '@cambrian/app/components/branding/Glow'
 import IndexHeaderSection from '@cambrian/app/components/sections/IndexHeaderSection'
 import PageLayout from '@cambrian/app/components/layout/PageLayout'
+import USPListSection from '@cambrian/app/components/sections/USPListSection'
 import USPSection from '@cambrian/app/components/sections/USPSection'
 import { useRef } from 'react'
 
@@ -16,7 +18,18 @@ export default function Home() {
             <Box style={{ position: 'relative', overflow: 'hidden' }} fill>
                 <IndexHeaderSection onClickCTA={handleClickHeaderCTA} />
                 <Box ref={startRef} />
-                <USPSection />
+                <Box style={{ position: 'relative', overflow: 'hidden' }}>
+                    <Glow
+                        height="1000px"
+                        width="1000px"
+                        left={'-40%'}
+                        top={'30%'}
+                    />
+                    <Box style={{ position: 'relative' }}>
+                        <USPSection />
+                        <USPListSection />
+                    </Box>
+                </Box>
             </Box>
         </PageLayout>
     )
