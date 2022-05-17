40fe831264b56798516afc4f32b08b0eb6c32a73
Hensohimself <oliver.hens@gmail.com>
2022-05-17 07:48:58 -0500

    Links updated

--- packages/app/src/components/layout/footer/FooterTermsSection.tsx
+++ packages/app/src/components/layout/footer/FooterTermsSection.tsx
@@ -1,7 +1,12 @@
+import { Anchor } from 'grommet'
 import { Text } from 'grommet'
 
 const FooterTermsSection = () => (
-    <Text size="small">We're not interested in your data 🚀</Text>
+    <Anchor href="https://www.notion.so/cambrianprotocol/Terms-of-Use-7d9eb8b5fa4144028b0b7a24df3e803b">
+        <Text size="small" color="dark-4">
+            Terms of Use
+        </Text>
+    </Anchor>
 )
 
 export default FooterTermsSection
