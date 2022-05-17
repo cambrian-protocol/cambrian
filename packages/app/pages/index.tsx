fe73323c656a37973fc6b6e446035f3c49bd2f8d
Hensohimself <oliver.hens@gmail.com>
2022-05-17 12:46:28 -0500

    Added RecommendedReadingsSection

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -2,6 +2,7 @@ import { Box } from 'grommet'
 import Glow from '@cambrian/app/components/branding/Glow'
 import IndexHeaderSection from '@cambrian/app/components/sections/IndexHeaderSection'
 import PageLayout from '@cambrian/app/components/layout/PageLayout'
+import RecommendedReadingsSection from '@cambrian/app/components/sections/RecommendedReadingsSection'
 import USPListSection from '@cambrian/app/components/sections/USPListSection'
 import USPSection from '@cambrian/app/components/sections/USPSection'
 import { useRef } from 'react'
@@ -25,9 +26,16 @@ export default function Home() {
                         left={'-40%'}
                         top={'30%'}
                     />
+                    <Glow
+                        height="1500px"
+                        width="1500px"
+                        left={'30%'}
+                        bottom={'0%'}
+                    />
                     <Box style={{ position: 'relative' }}>
                         <USPSection />
                         <USPListSection />
+                        <RecommendedReadingsSection />
                     </Box>
                 </Box>
             </Box>
