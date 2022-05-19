4f849592107e61920c6851f4e3f2d521ed0a744e
Hensohimself <oliver.hens@gmail.com>
2022-05-19 14:33:04 -0500

    Added CreateProposalMultiStepForm, minor style updates

---
+++ packages/app/src/ui/proposals/forms/steps/CreateProposalNotificationStep.tsx
@@ -0,0 +1,79 @@
+import {
+    CREATE_PROPOSAL_STEPS,
+    CreateProposalMultiStepFormType,
+    CreateProposalMultiStepStepsType,
+} from '../CreateProposalMultiStepForm'
+import { SetStateAction, useState } from 'react'
+
+import { Check } from 'phosphor-react'
+import DiscordWebhookInput from '@cambrian/app/components/inputs/DiscordWebhookInput'
+import { Form } from 'grommet'
+import HeaderTextSection from '@cambrian/app/components/sections/HeaderTextSection'
+import MultiStepFormLayout from '@cambrian/app/components/layout/MultiStepFormLayout'
+import MultiStepFormNav from '@cambrian/app/components/nav/MultiStepFormNav'
+
+interface CreateProposalNotificationStepProps {
+    input: CreateProposalMultiStepFormType
+    setInput: React.Dispatch<SetStateAction<CreateProposalMultiStepFormType>>
+    stepperCallback: (step: CreateProposalMultiStepStepsType) => void
+    createProposal: () => Promise<void>
+}
+type CreateProposalNotificationStepFormType = {
+    discordWebhook: string
+}
+const CreateProposalNotificationStep = ({
+    input,
+    setInput,
+    stepperCallback,
+    createProposal,
+}: CreateProposalNotificationStepProps) => {
+    const [notificationInput, setNotificationInput] =
+        useState<CreateProposalNotificationStepFormType>({
+            discordWebhook: input.discordWebhook || '',
+        })
+    const [isCreating, setIsCreating] = useState(false)
+
+    const onSubmit = async () => {
+        setIsCreating(true)
+        const updatedInput = { ...input, ...notificationInput }
+        setInput(updatedInput)
+        await createProposal()
+        setIsCreating(false)
+    }
+    return (
+        <Form<CreateProposalNotificationStepFormType>
+            onChange={(nextValue: CreateProposalNotificationStepFormType) => {
+                setNotificationInput(nextValue)
+            }}
+            value={notificationInput}
+        >
+            <MultiStepFormLayout
+                nav={
+                    <MultiStepFormNav
+                        submitForm={{
+                            isLoading: isCreating,
+                            onClick: onSubmit,
+                            label: 'Finish',
+                            primary: true,
+                            icon: <Check />,
+                        }}
+                        backward={() =>
+                            stepperCallback(CREATE_PROPOSAL_STEPS.FLEX_INPUTS)
+                        }
+                    />
+                }
+            >
+                <HeaderTextSection
+                    subTitle={`${input.flexInputs.length > 0 ? 5 : 4}/${
+                        input.flexInputs.length > 0 ? 5 : 4
+                    }`}
+                    title="Almost done, how can we notify you with updates about the proposal?"
+                    paragraph={`You can also join our Discord Server and use the /watch functionality to receive notification DMs. More on that as soon as you hit the Create Template Button. `}
+                />
+                <DiscordWebhookInput name="discordWebhook" />
+            </MultiStepFormLayout>
+        </Form>
+    )
+}
+
+export default CreateProposalNotificationStep
