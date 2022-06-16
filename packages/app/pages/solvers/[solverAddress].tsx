93a7271d3ce591c68cb77dc5367504951bcc4a31
Hensohimself <oliver.hens@gmail.com>
2022-06-16 11:23:41 -0500

    Solver Loading Flicker removed

--- packages/app/pages/solvers/[solverAddress].tsx
+++ packages/app/pages/solvers/[solverAddress].tsx
@@ -18,8 +18,7 @@ export default function SolverPage() {
     const { solverAddress } = router.query
 
     const [solverContract, setSolverContract] = useState<ethers.Contract>()
-    const [showInvalidQueryComponent, setShowInvalidQueryComponent] =
-        useState(false)
+    const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
         if (!router.isReady) return
@@ -28,47 +27,49 @@ export default function SolverPage() {
     }, [currentUser, router])
 
     const initSolverContract = async () => {
-        try {
-            if (
-                !solverAddress ||
-                typeof solverAddress !== 'string' ||
-                !currentUser.chainId ||
-                !SUPPORTED_CHAINS[currentUser.chainId] ||
-                !ethers.utils.isAddress(solverAddress)
-            )
-                throw new Error()
+        if (currentUser.signer && currentUser.chainId) {
+            try {
+                if (
+                    !solverAddress ||
+                    typeof solverAddress !== 'string' ||
+                    !ethers.utils.isAddress(solverAddress) ||
+                    !SUPPORTED_CHAINS[currentUser.chainId]
+                )
+                    throw new Error()
 
-            const contract = new ethers.Contract(
-                solverAddress,
-                BASE_SOLVER_IFACE,
-                currentUser.signer
-            )
+                const contract = new ethers.Contract(
+                    solverAddress,
+                    BASE_SOLVER_IFACE,
+                    currentUser.signer
+                )
 
-            // Check if we actually received a Solver
-            await contract.trackingId()
-            setSolverContract(contract)
-        } catch (e) {
-            setShowInvalidQueryComponent(true)
+                // Check if we actually received a Solver
+                await contract.trackingId()
+                setSolverContract(contract)
+            } catch (e) {}
+            setIsLoaded(true)
         }
     }
 
     return (
         <>
             {currentUser.signer ? (
-                solverContract ? (
-                    <Solver
-                        solverContract={solverContract}
-                        currentUser={currentUser}
-                    />
-                ) : showInvalidQueryComponent ? (
-                    <PageLayout contextTitle="Solver">
-                        <InvalidQueryComponent context="Solver" />
-                    </PageLayout>
+                isLoaded ? (
+                    solverContract ? (
+                        <Solver
+                            solverContract={solverContract}
+                            currentUser={currentUser}
+                        />
+                    ) : (
+                        <PageLayout contextTitle="Solver">
+                            <InvalidQueryComponent context="Solver" />
+                        </PageLayout>
+                    )
                 ) : (
                     <LoadingScreen context={LOADING_MESSAGE['SOLVER']} />
                 )
             ) : (
-                <PageLayout contextTitle="Solver">
+                <PageLayout contextTitle="Connect your Wallet">
                     <ConnectWalletSection />
                 </PageLayout>
             )}
