1e1d9d1b2dc1b23ec88eb243d29b6c2df10a54b6
Nic Wickman <nicwickman@gmail.com>
2022-08-01 18:47:51 -0700

    https ceramic node

--- packages/app/config/index.ts
+++ packages/app/config/index.ts
@@ -16,7 +16,7 @@ export const IPFS_GATEWAYS = [
 ]
 
 // Ceramic
-export const CERAMIC_NODE_ENDPOINT = 'http://ceramic.cambrianprotocol.com:7007'
+export const CERAMIC_NODE_ENDPOINT = 'https://ceramic.cambrianprotocol.com'
 
 // BE Endpoints
 export const WEBHOOK_API_ENDPOINT =
