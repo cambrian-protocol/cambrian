042f97da6f63b7e7369446b28fb65fd2e979d0f7
Hensohimself <oliver.hens@gmail.com>
2022-09-20 09:12:44 +0200

    Open new tab on USP click

--- packages/app/src/components/cards/USPCard.tsx
+++ packages/app/src/components/cards/USPCard.tsx
@@ -16,35 +16,41 @@ const USPCard = ({ icon, title, description, href }: USPCardProps) => {
     return (
         <Box pad="medium">
             <Link href={href}>
-                <Card
-                    hoverIndicator={{ background: 'background-contrast' }}
-                    elevation="small"
-                    pad={{ vertical: 'large', horizontal: 'medium' }}
-                    round="xsmall"
-                    height="medium"
-                    width={'medium'}
-                    background="background-back"
-                    border
-                    justify="around"
-                    align="center"
-                    gap="large"
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: 'none' }}
                 >
-                    <IconContext.Provider value={{ size: '48' }}>
-                        {icon}
-                    </IconContext.Provider>
-                    <Box gap="small">
-                        <Text size="large" textAlign="center">
-                            {title}
-                        </Text>
-                        <Text textAlign="center" color="dark-4">
-                            {description}
-                        </Text>
-                    </Box>
-                    <Box direction="row" gap="small">
-                        <Text textAlign="center">Learn more</Text>
-                        <ArrowCircleRight size="24" />
-                    </Box>
-                </Card>
+                    <Card
+                        hoverIndicator={{ background: 'background-contrast' }}
+                        elevation="small"
+                        pad={{ vertical: 'large', horizontal: 'medium' }}
+                        round="xsmall"
+                        height="medium"
+                        width={'medium'}
+                        background="background-back"
+                        border
+                        justify="around"
+                        align="center"
+                        gap="large"
+                    >
+                        <IconContext.Provider value={{ size: '48' }}>
+                            {icon}
+                        </IconContext.Provider>
+                        <Box gap="small">
+                            <Text size="large" textAlign="center">
+                                {title}
+                            </Text>
+                            <Text textAlign="center" color="dark-4">
+                                {description}
+                            </Text>
+                        </Box>
+                        <Box direction="row" gap="small">
+                            <Text textAlign="center">Learn more</Text>
+                            <ArrowCircleRight size="24" />
+                        </Box>
+                    </Card>
+                </a>
             </Link>
         </Box>
     )
