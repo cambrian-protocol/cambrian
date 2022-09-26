d7bd0e9f4180c83aac94e8c98323e47706c29ef8
Hensohimself <oliver.hens@gmail.com>
2022-09-26 14:17:22 +0200

    Stage fetching clean up, solver url refactor to one url (all stages are accesible via solvers/[id], links not wired yet)

--- packages/app/pages/templates/[templateStreamID].tsx
+++ packages/app/pages/templates/[templateStreamID].tsx
@@ -1,40 +1,57 @@
+import { useEffect, useState } from 'react'
+
 import Custom404Page from '../404'
-import ErrorPopupModal from '@cambrian/app/components/modals/ErrorPopupModal'
-import PageLayout from '@cambrian/app/components/layout/PageLayout'
+import { LOADING_MESSAGE } from '@cambrian/app/constants/LoadingMessages'
+import LoadingScreen from '@cambrian/app/components/info/LoadingScreen'
+import { StageNames } from '@cambrian/app/models/StageModel'
+import { TemplateModel } from '@cambrian/app/models/TemplateModel'
 import TemplateUI from '@cambrian/app/ui/templates/TemplateUI'
-import useEditTemplate from '@cambrian/app/hooks/useEditTemplate'
+import { TileDocument } from '@ceramicnetwork/stream-tile'
+import { UserType } from '@cambrian/app/store/UserContext'
+import { fetchStageTileDoc } from '@cambrian/app/utils/helpers/stageHelper'
+import { useCurrentUserContext } from '@cambrian/app/hooks/useCurrentUserContext'
+import { useRouter } from 'next/router'
 
 export default function ViewTemplatePage() {
-    const {
-        templateInput,
-        templateStreamID,
-        errorMessage,
-        setErrorMessage,
-        show404NotFound,
-        composition,
-    } = useEditTemplate()
+    const router = useRouter()
+    const { templateStreamID } = router.query
+    const { currentUser } = useCurrentUserContext()
+    const [templateStreamDoc, setTemplateStreamDoc] =
+        useState<TileDocument<TemplateModel>>()
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        if (router.isReady && currentUser) fetchTemplate(currentUser)
+    }, [router, currentUser])
+
+    const fetchTemplate = async (currentUser: UserType) => {
+        try {
+            setTemplateStreamDoc(
+                (await fetchStageTileDoc(
+                    currentUser,
+                    templateStreamID as string,
+                    StageNames.template
+                )) as TileDocument<TemplateModel>
+            )
+        } catch (e) {
+            console.error(e)
+        }
+        setIsLoaded(true)
+    }
 
     return (
         <>
-            {show404NotFound ? (
-                <Custom404Page />
-            ) : (
-                <PageLayout
-                    contextTitle={templateInput?.title || 'Template'}
-                    kind="narrow"
-                >
+            {isLoaded ? (
+                templateStreamDoc && currentUser ? (
                     <TemplateUI
-                        composition={composition}
-                        ceramicTemplate={templateInput}
-                        templateStreamID={templateStreamID}
+                        currentUser={currentUser}
+                        templateStreamDoc={templateStreamDoc}
                     />
-                </PageLayout>
-            )}
-            {errorMessage && (
-                <ErrorPopupModal
-                    onClose={() => setErrorMessage(undefined)}
-                    errorMessage={errorMessage}
-                />
+                ) : (
+                    <Custom404Page />
+                )
+            ) : (
+                <LoadingScreen context={LOADING_MESSAGE['TEMPLATE']} />
             )}
         </>
     )
--- packages/app/src/store/ProposalContext.tsx
+++ packages/app/src/store/ProposalContext.tsx
@@ -35,7 +35,7 @@ export type ProposalContextType = {
 }
 
 type ProposalProviderProps = PropsWithChildren<{}> & {
-    proposalStreamID: string
+    proposalStreamDoc: TileDocument<ProposalModel>
     currentUser: UserType
 }
 
@@ -45,21 +45,16 @@ export const ProposalContext = React.createContext<ProposalContextType>({
 })
 
 export const ProposalContextProvider: React.FunctionComponent<ProposalProviderProps> =
-    ({ proposalStreamID, currentUser, children }) => {
+    ({ proposalStreamDoc, currentUser, children }) => {
         const proposalsHub = new ProposalsHub(
             currentUser.signer,
             currentUser.chainId
         )
-        const ceramicTemplateAPI = new CeramicTemplateAPI(currentUser)
-
         const [proposalStatus, setProposalStatus] = useState<ProposalStatus>()
         const [stageStack, setStageStack] = useState<StageStackType>()
         const [onChainProposal, setOnChainProposal] =
             useState<ethers.Contract>()
         const [isLoaded, setIsLoaded] = useState(false)
-
-        const [proposalStreamDoc, setProposalStreamDoc] =
-            useState<TileDocument<ProposalModel>>()
         const [templateStreamDoc, setTemplateStreamDoc] =
             useState<TileDocument<TemplateModel>>()
 
@@ -94,7 +89,7 @@ export const ProposalContextProvider: React.FunctionComponent<ProposalProviderPr
                                     undefined,
                                     onChainProposal,
                                     templateStreamDoc.content.receivedProposals[
-                                        proposalStreamID
+                                        proposalStreamDoc.id.toString()
                                     ]
                                 )
                             )
@@ -120,7 +115,10 @@ export const ProposalContextProvider: React.FunctionComponent<ProposalProviderPr
 
         const updateProposalLib = async () => {
             if (stageStack && proposalStatus !== ProposalStatus.Unknown) {
-                await addRecentStage(currentUser, proposalStreamID)
+                await addRecentStage(
+                    currentUser,
+                    proposalStreamDoc.id.toString()
+                )
             }
         }
 
@@ -154,21 +152,13 @@ export const ProposalContextProvider: React.FunctionComponent<ProposalProviderPr
 
         const init = async () => {
             try {
-                const _proposalStreamDoc = await loadStageDoc<ProposalModel>(
-                    currentUser,
-                    proposalStreamID
-                )
-
-                _proposalStreamDoc.makeReadOnly()
-
                 const _templateStreamDoc = await loadStageDoc<TemplateModel>(
                     currentUser,
-                    _proposalStreamDoc.content.template.streamID
+                    proposalStreamDoc.content.template.streamID
                 )
 
-                await initProposal(_proposalStreamDoc, _templateStreamDoc)
+                await initProposal(proposalStreamDoc, _templateStreamDoc)
 
-                setProposalStreamDoc(_proposalStreamDoc)
                 setTemplateStreamDoc(_templateStreamDoc)
                 setIsLoaded(true)
             } catch (e) {
@@ -181,6 +171,8 @@ export const ProposalContextProvider: React.FunctionComponent<ProposalProviderPr
             templateStreamDoc: TileDocument<TemplateModel>
         ) => {
             try {
+                const proposalStreamID = proposalStreamDoc.id.toString()
+
                 const cambrianStageStackDoc = (await TileDocument.deterministic(
                     ceramicInstance(currentUser),
                     {
@@ -245,6 +237,9 @@ export const ProposalContextProvider: React.FunctionComponent<ProposalProviderPr
                         stageStack.template.author === currentUser.did &&
                         stageStack.proposal.isSubmitted
                     ) {
+                        const ceramicTemplateAPI = new CeramicTemplateAPI(
+                            currentUser
+                        )
                         receivedProposals =
                             await ceramicTemplateAPI.registerNewProposalSubmission(
                                 stageStack
