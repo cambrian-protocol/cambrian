b1ea4532b40262db91f89dccda48194c88959441
Hensohimself <oliver.hens@gmail.com>
2022-09-29 09:59:53 +0200

    getRedeemable performance refactor

--- packages/app/pages/safe/index.tsx
+++ packages/app/pages/safe/index.tsx
@@ -1,9 +1,14 @@
 import { BigNumber, ethers } from 'ethers'
 import { Box, Text } from 'grommet'
+import {
+    getCollectionId,
+    getPositionId,
+} from '@cambrian/app/utils/helpers/ctHelpers'
 import { useCallback, useEffect, useState } from 'react'
 
 import { BASE_SOLVER_IFACE } from 'packages/app/config/ContractInterfaces'
 import CTFContract from '@cambrian/app/contracts/CTFContract'
+import { ConditionStatus } from '@cambrian/app/models/ConditionStatus'
 import DashboardHeader from '@cambrian/app/components/layout/header/DashboardHeader'
 import { INFURA_ID } from '../../config'
 import LoaderButton from '@cambrian/app/components/buttons/LoaderButton'
@@ -18,7 +23,6 @@ import { fetchTokenInfo } from '@cambrian/app/utils/helpers/tokens'
 import { getIndexSetFromBinaryArray } from '@cambrian/app/utils/transformers/ComposerTransformer'
 import { loadStageStackFromID } from '@cambrian/app/services/ceramic/CeramicUtils'
 import { useCurrentUserContext } from '@cambrian/app/hooks/useCurrentUserContext'
-import { getCollectionId } from '@cambrian/app/utils/helpers/ctHelpers'
 
 const providerOptions = {
     injected: {
@@ -61,6 +65,15 @@ type RedeemablePosition = {
 
 type RedeemablePositionsHash = { [positionId: string]: RedeemablePosition }
 
+type PositionSolverInfoType = {
+    collateralToken: TokenModel
+    parentCollectionId: string
+    conditionId: string
+    partition: number[]
+    solverMetadata?: SolverMetadataModel
+    positionId: string
+}
+
 export default function Safe() {
     const { currentUser } = useCurrentUserContext()
     const [connectedWallet, setConnectedWallet] = useState<ConnectedWallet>()
@@ -101,104 +114,17 @@ export default function Safe() {
         if (!ctf || !connectedWallet) {
             return
         }
-        // Find all the conditional tokens that have been sent to our connected user's address
-        const transferBatchFilter = ctf.contract.filters.TransferBatch(
-            null, // operator
-            null, // from
-            connectedWallet.address // to
-        )
 
-        const transferBatchLogs = await ctf.contract.queryFilter(
-            transferBatchFilter
-        )
-
-        const collateralTokensCache: string[] = []
-        const solverCollateralTokenCache: {
-            [solverAddress: string]: {
-                solverContract: ethers.Contract
-                collateralToken: string
-                partition: number[]
-            }
+        const redemptionCache: { [conditionId: string]: boolean } = {}
+        const solverCache: {
+            [solverAddress: string]: PositionSolverInfoType
         } = {}
-        const positionsCache: {
-            [positionId: string]: {
+        const positions: {
+            [positionId: string]: PositionSolverInfoType & {
                 amount: BigNumber
                 solverAddress: string
-                solverContract: ethers.Contract
-                partition: number[]
             }
         } = {}
-
-        await Promise.all(
-            transferBatchLogs.map(async (log) => {
-                const solverAddress = log.args![0]
-                const positionIds: BigNumber[] = log.args![3]
-                const values: BigNumber[] = log.args![4]
-                let collateralToken: string
-                let solverContract: ethers.Contract
-                let partition: number[]
-
-                if (!solverCollateralTokenCache[solverAddress]) {
-                    try {
-                        const newSolverContract = new ethers.Contract(
-                            solverAddress,
-                            BASE_SOLVER_IFACE,
-                            connectedWallet.signer
-                        )
-                        const solverConfig = await newSolverContract.getConfig()
-
-                        collateralToken =
-                            solverConfig.conditionBase.collateralToken
-                        solverContract = newSolverContract
-                        partition = solverConfig.conditionBase.partition
-
-                        if (!collateralTokensCache.includes(collateralToken))
-                            collateralTokensCache.push(collateralToken)
-
-                        solverCollateralTokenCache[solverAddress] = {
-                            collateralToken: collateralToken,
-                            solverContract: solverContract,
-                            partition: partition,
-                        }
-                    } catch (e) {
-                        throw new Error('No Solver Config found!')
-                    }
-                } else {
-                    solverContract =
-                        solverCollateralTokenCache[solverAddress].solverContract
-                    collateralToken =
-                        solverCollateralTokenCache[solverAddress]
-                            .collateralToken
-                    partition =
-                        solverCollateralTokenCache[solverAddress].partition
-                }
-
-                positionIds.forEach((positionId, idx) => {
-                    const positionIdHex = positionId.toHexString()
-                    if (positionsCache[positionIdHex]) {
-                        // Just add amount
-                        positionsCache[positionIdHex] = {
-                            ...positionsCache[positionIdHex],
-                            amount: positionsCache[positionIdHex].amount.add(
-                                values[idx]
-                            ),
-                        }
-                    } else {
-                        positionsCache[positionIdHex] = {
-                            solverAddress: solverAddress,
-                            amount: values[idx],
-                            solverContract: solverContract,
-                            partition: partition,
-                        }
-                    }
-                })
-            })
-        )
-
-        // For getting resolved conditions
-        const resolvedConditionLogs = await ctf.contract.queryFilter(
-            ctf.contract.filters.ConditionResolution(null, null, null)
-        )
 
         // For getting payouts the user has already redeemed
         const payoutRedemptionLogs = await ctf.contract.queryFilter(
@@ -209,61 +135,74 @@ export default function Safe() {
             )
         )
 
-        // Filter out already redeemed conditions
-        const redeemableConditionLogs = resolvedConditionLogs.filter(
-            (resolvedCondition) =>
-                payoutRedemptionLogs.find(
-                    (redeemedPayoutLog) =>
-                        redeemedPayoutLog.args![3] ===
-                        resolvedCondition.args![0]
-                ) === undefined
+        // Hash with already redeemed conditionId's
+        payoutRedemptionLogs.forEach((log) => {
+            if (!redemptionCache[log.args![3]])
+                redemptionCache[log.args![3]] = true
+        })
+
+        // Find all the conditional tokens that have ever been sent to our connected user's address
+        const transferBatchFilter = ctf.contract.filters.TransferBatch(
+            null, // operator
+            null, // from
+            connectedWallet.address // to
         )
 
-        const _redeemablePositions: RedeemablePositionsHash = {}
+        const transferBatchLogs = await ctf.contract.queryFilter(
+            transferBatchFilter
+        )
 
-        // TODO replace getPositionId and getCollectionId with helper functions from gnosis ctf
         await Promise.all(
-            redeemableConditionLogs.map(async (redeemableCondition) => {
-                const conditionId = redeemableCondition.args!.conditionId
-                const indexSet = getIndexSetFromBinaryArray(
-                    redeemableCondition.args!.payoutNumerators
-                )
-                const collectionId = await ctf.contract.getCollectionId(
-                    ethers.constants.HashZero,
-                    conditionId,
-                    indexSet
-                )
+            transferBatchLogs.map(async (log) => {
+                const solverAddress = log.args![0]
+                const positionIds: BigNumber[] = log.args![3]
+                const values: BigNumber[] = log.args![4]
 
-                console.log('onChain collectionId: ', collectionId)
-                console.log(
-                    'manual collectionId',
-                    getCollectionId(conditionId, indexSet)
-                )
-
-                await Promise.all(
-                    collateralTokensCache.map(
-                        async (collateralTokenAddress) => {
-                            const positionId: BigNumber =
-                                await ctf.contract.getPositionId(
-                                    collateralTokenAddress,
-                                    collectionId
+                if (!solverCache[solverAddress]) {
+                    try {
+                        const newSolverContract = new ethers.Contract(
+                            solverAddress,
+                            BASE_SOLVER_IFACE,
+                            connectedWallet.signer
+                        )
+                        const solverConfig = await newSolverContract.getConfig()
+                        const allConditions =
+                            await newSolverContract.getConditions()
+                        const latestCondition =
+                            allConditions[allConditions.length - 1]
+                        // Check if already redeemed
+                        if (!redemptionCache[latestCondition.conditionId]) {
+                            const solverStatus =
+                                await newSolverContract.getStatus(
+                                    allConditions.length - 1
                                 )
-                            const positionHex = positionId.toHexString()
-
-                            // Cross-ref calculated positionId with cached position
-                            if (positionsCache[positionHex]) {
+                            if (
+                                solverStatus ===
+                                    ConditionStatus.OutcomeReported ||
+                                solverStatus ===
+                                    ConditionStatus.ArbitrationDelivered
+                            ) {
                                 const collateralToken = await fetchTokenInfo(
-                                    collateralTokenAddress,
+                                    solverConfig.conditionBase.collateralToken,
                                     connectedWallet.web3Provider
+                                )
+
+                                const positionId = getPositionId(
+                                    collateralToken.address,
+                                    getCollectionId(
+                                        latestCondition.conditionId,
+                                        getIndexSetFromBinaryArray(
+                                            latestCondition.payouts
+                                        )
+                                    )
                                 )
 
                                 let solverMetadata:
                                     | SolverMetadataModel
                                     | undefined = undefined
                                 // Fetch Solver Metadata
-                                const proposalId = await positionsCache[
-                                    positionHex
-                                ].solverContract.trackingId()
+                                const proposalId =
+                                    await newSolverContract.trackingId()
                                 if (proposalId && currentUser) {
                                     const proposalsHub = new ProposalsHub(
                                         connectedWallet.signer,
@@ -282,9 +221,7 @@ export default function Safe() {
 
                                     if (stageStack) {
                                         const solverIndex =
-                                            (await positionsCache[
-                                                positionHex
-                                            ].solverContract.chainIndex()) as
+                                            (await newSolverContract.chainIndex()) as
                                                 | number
                                                 | undefined
                                         if (solverIndex !== undefined) {
@@ -302,22 +239,50 @@ export default function Safe() {
                                         }
                                     }
                                 }
-                                _redeemablePositions[positionHex] = {
-                                    ...positionsCache[positionHex],
+                                solverCache[solverAddress] = {
                                     collateralToken: collateralToken,
-                                    conditionId: conditionId,
+                                    partition:
+                                        solverConfig.conditionBase.partition,
+                                    conditionId: latestCondition.conditionId,
                                     parentCollectionId:
-                                        ethers.constants.HashZero,
+                                        latestCondition.parentCollectionId,
                                     solverMetadata: solverMetadata,
+                                    positionId: positionId,
                                 }
                             }
                         }
-                    )
-                )
+                    } catch (e) {
+                        throw e
+                    }
+                }
+                if (solverCache[solverAddress]) {
+                    positionIds.forEach((positionId, idx) => {
+                        const positionIdHex = positionId.toHexString()
+                        if (
+                            solverCache[solverAddress].positionId ===
+                            positionIdHex
+                        ) {
+                            if (positions[positionIdHex]) {
+                                // Just add amount
+                                positions[positionIdHex] = {
+                                    ...positions[positionIdHex],
+                                    amount: positions[positionIdHex].amount.add(
+                                        values[idx]
+                                    ),
+                                }
+                            } else {
+                                positions[positionIdHex] = {
+                                    ...solverCache[solverAddress],
+                                    solverAddress: solverAddress,
+                                    amount: values[idx],
+                                }
+                            }
+                        }
+                    })
+                }
             })
         )
-
-        setRedeemablePositions(_redeemablePositions)
+        setRedeemablePositions(positions)
     }
 
     const onRedeem = async (redeemablePosition: RedeemablePosition) => {
