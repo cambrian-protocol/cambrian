d9ba7c0eb2f3a5895d547974701f5e3c5d298f3b
Hensohimself <oliver.hens@gmail.com>
2022-09-29 12:56:17 +0200

    RedeemableTokenListWidget extraction

--- packages/app/pages/safe/index.tsx
+++ packages/app/pages/safe/index.tsx
@@ -1,16 +1,10 @@
-import { Box, Text } from 'grommet'
-import {
-    RedeemablePosition,
-    RedeemablePositionsHash,
-    getRedeemablePositions,
-} from '@cambrian/app/utils/helpers/redeemHelper'
 import { useCallback, useEffect, useState } from 'react'
 
-import CTFContract from '@cambrian/app/contracts/CTFContract'
+import { Box } from 'grommet'
 import DashboardHeader from '@cambrian/app/components/layout/header/DashboardHeader'
 import { INFURA_ID } from '../../config'
-import LoaderButton from '@cambrian/app/components/buttons/LoaderButton'
 import PageLayout from '@cambrian/app/components/layout/PageLayout'
+import RedeemableTokenListWidget from '@cambrian/app/ui/dashboard/widgets/RedeemableTokenListWidget'
 import { SafeAppWeb3Modal } from '@gnosis.pm/safe-apps-web3modal'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import { cpLogger } from '@cambrian/app/services/api/Logger.api'
@@ -47,45 +41,10 @@ type ConnectedWallet = {
 
 export default function Safe() {
     const [connectedWallet, setConnectedWallet] = useState<ConnectedWallet>()
-    const [redeemablePositions, setRedeemablePositions] =
-        useState<RedeemablePositionsHash>({})
 
     useEffect(() => {
         connectWallet()
     }, [])
-
-    useEffect(() => {
-        if (connectedWallet) init(connectedWallet)
-    }, [connectedWallet])
-
-    const init = async (connectedWallet: ConnectedWallet) => {
-        setRedeemablePositions(
-            await getRedeemablePositions(
-                connectedWallet.address,
-                connectedWallet.web3Provider,
-                connectedWallet.network.chainId
-            )
-        )
-    }
-
-    const onRedeem = async (redeemablePosition: RedeemablePosition) => {
-        if (connectedWallet) {
-            try {
-                const ctfContract = new CTFContract(
-                    connectedWallet.web3Provider,
-                    connectedWallet.network.chainId
-                )
-                await ctfContract.contract.redeemPositions(
-                    redeemablePosition.collateralToken.address,
-                    redeemablePosition.parentCollectionId,
-                    redeemablePosition.conditionId,
-                    redeemablePosition.partition
-                )
-            } catch (e) {
-                console.error(e)
-            }
-        }
-    }
 
     const connectWallet = useCallback(async function () {
         try {
@@ -114,59 +73,13 @@ export default function Safe() {
                     title="Redeem your token"
                     description="Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat."
                 />
-                {Object.keys(redeemablePositions).map((positionId, idx) => {
-                    const redeemablePosition = redeemablePositions[positionId]
-                    const formattedAmount = ethers.utils.formatUnits(
-                        redeemablePositions[positionId].amount,
-                        redeemablePosition.collateralToken.decimals
-                    )
-                    return (
-                        <Box
-                            key={positionId}
-                            pad="small"
-                            border
-                            round="xsmall"
-                            direction="row"
-                        >
-                            <Box
-                                flex
-                                direction="row"
-                                justify="between"
-                                pad={{
-                                    left: 'small',
-                                    right: 'medium',
-                                    vertical: 'small',
-                                }}
-                                align="center"
-                            >
-                                <Box>
-                                    <Text>
-                                        {redeemablePosition.solverMetadata
-                                            ?.solverTag.title || 'Solver'}
-                                    </Text>
-                                    <Text color={'dark-4'} size="small">
-                                        {
-                                            redeemablePositions[positionId]
-                                                .solverAddress
-                                        }
-                                    </Text>
-                                </Box>
-                                <Text>
-                                    {formattedAmount}{' '}
-                                    {redeemablePosition.collateralToken.symbol}
-                                </Text>
-                            </Box>
-                            <Box justify="center">
-                                <LoaderButton
-                                    primary
-                                    onClick={() => onRedeem(redeemablePosition)}
-                                    isLoading={false}
-                                    label="Redeem"
-                                />
-                            </Box>
-                        </Box>
-                    )
-                })}
+                {connectedWallet && (
+                    <RedeemableTokenListWidget
+                        signerOrProvider={connectedWallet.signer}
+                        address={connectedWallet.address}
+                        chainId={connectedWallet.network.chainId}
+                    />
+                )}
             </Box>
         </PageLayout>
     )
---
+++ packages/app/src/ui/dashboard/widgets/RedeemableTokenListWidget.tsx
@@ -0,0 +1,115 @@
+import { Box, Text } from 'grommet'
+import {
+    RedeemablePosition,
+    RedeemablePositionsHash,
+    getRedeemablePositions,
+} from '@cambrian/app/utils/helpers/redeemHelper'
+import { useEffect, useState } from 'react'
+
+import CTFContract from '@cambrian/app/contracts/CTFContract'
+import LoaderButton from '@cambrian/app/components/buttons/LoaderButton'
+import { ethers } from 'ethers'
+
+interface RedeemableTokensWidgetProps {
+    address: string
+    signerOrProvider: ethers.Signer | ethers.providers.Provider
+    chainId: number
+}
+
+const RedeemableTokenListWidget = ({
+    address,
+    signerOrProvider,
+    chainId,
+}: RedeemableTokensWidgetProps) => {
+    const [redeemablePositions, setRedeemablePositions] =
+        useState<RedeemablePositionsHash>({})
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        init()
+    }, [])
+
+    const init = async () => {
+        try {
+            setIsLoading(true)
+            setRedeemablePositions(
+                await getRedeemablePositions(address, signerOrProvider, chainId)
+            )
+        } catch (e) {}
+        setIsLoading(false)
+    }
+
+    const onRedeem = async (redeemablePosition: RedeemablePosition) => {
+        try {
+            const ctfContract = new CTFContract(signerOrProvider, chainId)
+            await ctfContract.contract.redeemPositions(
+                redeemablePosition.collateralToken.address,
+                redeemablePosition.parentCollectionId,
+                redeemablePosition.conditionId,
+                redeemablePosition.partition
+            )
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    return (
+        <Box gap="small">
+            {Object.keys(redeemablePositions).map((positionId, idx) => {
+                const redeemablePosition = redeemablePositions[positionId]
+                const formattedAmount = ethers.utils.formatUnits(
+                    redeemablePositions[positionId].amount,
+                    redeemablePosition.collateralToken.decimals
+                )
+                return (
+                    <Box
+                        key={positionId}
+                        pad="small"
+                        border
+                        round="xsmall"
+                        direction="row"
+                    >
+                        <Box
+                            flex
+                            direction="row"
+                            justify="between"
+                            pad={{
+                                left: 'small',
+                                right: 'medium',
+                                vertical: 'small',
+                            }}
+                            align="center"
+                        >
+                            <Box>
+                                <Text>
+                                    {redeemablePosition.solverMetadata
+                                        ?.solverTag.title || 'Solver'}
+                                </Text>
+                                <Text color={'dark-4'} size="small">
+                                    {
+                                        redeemablePositions[positionId]
+                                            .solverAddress
+                                    }
+                                </Text>
+                            </Box>
+                            <Text>
+                                {formattedAmount}{' '}
+                                {redeemablePosition.collateralToken.symbol}
+                            </Text>
+                        </Box>
+                        <Box justify="center">
+                            <LoaderButton
+                                primary
+                                onClick={() => onRedeem(redeemablePosition)}
+                                isLoading={false}
+                                label="Redeem"
+                            />
+                        </Box>
+                    </Box>
+                )
+            })}
+        </Box>
+    )
+}
+
+export default RedeemableTokenListWidget
