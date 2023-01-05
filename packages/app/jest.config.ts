280a5ad15a6dd7ea5affc7e44ed32ccab386afbc
Hensohimself <oliver.hens@gmail.com>
2023-01-05 13:24:06 +0000

    Fixed paths for old tests

--- packages/app/jest.config.ts
+++ packages/app/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
     moduleNameMapper: {
         '@cambrian/app/src/(.*)': '<rootDir>/src/$1',
-        '@cambrian/app/src/models/(.*)': '<rootDir>/src/models/$1',
+        '@cambrian/app/models/(.*)': '<rootDir>/src/models/$1',
         '@cambrian/app/src/ui/(.*)': '<rootDir>/src/ui/$1',
         '@cambrian/app/src/components/(.*)': '<rootDir>/src/components/$1',
-        '@cambrian/app/src/store/(.*)': '<rootDir>/src/store/$1',
+        '@cambrian/app/store/(.*)': '<rootDir>/src/store/$1',
         '@cambrian/app/utils/(.*)': '<rootDir>/src/utils/$1',
         '@cambrian/app/classes/(.*)': '<rootDir>/src/classes/$1',
         '@cambrian/app/src/theme/(.*)': '<rootDir>/src/theme/$1',
