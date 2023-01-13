862447a96c10a85bb81aa4693705056d6e12bf5a
Hensohimself <oliver.hens@gmail.com>
2023-01-13 09:07:35 +0000

    Removed auto height

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -93,7 +93,6 @@ export default function Home() {
                                         elevation="large"
                                         border
                                         overflow="hidden"
-                                        height={'auto'}
                                     >
                                         <Image
                                             fit="contain"
