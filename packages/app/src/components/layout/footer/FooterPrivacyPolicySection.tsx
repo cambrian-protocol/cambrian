9b504320cdb20f7618d771aae0496b5239f41f89
Nic Wickman <nicwickman@gmail.com>
2023-01-30 17:55:33 -0800

    privacy policy

--- packages/app/src/components/layout/footer/FooterPrivacyPolicySection.tsx
+++ packages/app/src/components/layout/footer/FooterPrivacyPolicySection.tsx
@@ -1,10 +1,12 @@
-import { Text } from 'grommet'
+import { Anchor, Text } from 'grommet'
 
 const FooterPrivacyPolicySection = () => {
     return (
-        <Text size="small" color="dark-4">
-            We're not interested in your data 🚀
-        </Text>
+        <Anchor href="https://www.notion.so/cambrianprotocol/Privacy-Policy-ef3c6fbc3fc74cb5a593f849cb4af54b">
+            <Text size="small" color="dark-4">
+                Privacy Policy
+            </Text>
+        </Anchor>
     )
 }
 
--- packages/app/src/components/layout/footer/FooterTermsSection.tsx
+++ packages/app/src/components/layout/footer/FooterTermsSection.tsx
@@ -2,11 +2,13 @@ import { Anchor } from 'grommet'
 import { Text } from 'grommet'
 
 const FooterTermsSection = () => (
-    <Anchor href="https://www.notion.so/cambrianprotocol/Terms-of-Use-7d9eb8b5fa4144028b0b7a24df3e803b">
-        <Text size="small" color="dark-4">
-            Terms of Use
-        </Text>
-    </Anchor>
+    <>
+        <Anchor href="https://www.notion.so/cambrianprotocol/Terms-of-Use-7d9eb8b5fa4144028b0b7a24df3e803b">
+            <Text size="small" color="dark-4">
+                Terms of Use
+            </Text>
+        </Anchor>
+    </>
 )
 
 export default FooterTermsSection
