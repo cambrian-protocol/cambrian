7e40337e6fe5787d6e7498474af6814900a147f8
Nic Wickman <nicwickman@gmail.com>
2023-02-02 15:26:33 -0800

    cors

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -6,7 +6,7 @@ module.exports = {
                 headers: [
                     {
                         key: 'Access-Control-Allow-Origin',
-                        value: 'https://cambrianprotocol.com',
+                        value: '*',
                     },
                     {
                         key: 'Access-Control-Allow-Methods',
