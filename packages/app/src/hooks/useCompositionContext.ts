15b8566e500c05d52f61734a1d83e300bcfaa651
Hensohimself <oliver.hens@gmail.com>
2023-03-08 11:22:11 +0000

    Clean up

--- packages/app/src/hooks/useCompositionContext.ts
+++
@@ -1,7 +0,0 @@
-import { CompositionContext, CompositionContextType } from './../store/composition.context';
-
-import { useContext } from 'react'
-
-export const useCompositionContext = () => {
-    return useContext<CompositionContextType>(CompositionContext)
-}
